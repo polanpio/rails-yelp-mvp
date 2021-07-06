5402597b0bacbe3f629b941b5e62c2d1e4ae6f47
Piotr Polanski <polanski.pio@gmail.com>
2021-07-06 19:56:57 +0900

    added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
